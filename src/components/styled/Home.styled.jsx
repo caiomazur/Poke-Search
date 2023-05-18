9fb9f75a8ae6d4b4ebb8658025f31acbbc837751
Caio Mazur <caiomazur1@gmail.com>
2023-05-18 13:27:24 +0100

    Fixed Public directory configuration

--- src/components/styled/Home.styled.jsx
+++ src/components/styled/Home.styled.jsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHome = styled.section`
-
-background-image: url("/src/assets/pattern_circles-7_1_2_0-0_0_1__ffffff_dedede.png");
+  background-image: url("/images7pattern_circles-7_1_2_0-0_0_1__ffffff_dedede.png");
   background-repeat: repeat-y;
   background-repeat: repeat-x;
   display: flex;
@@ -20,20 +19,19 @@ background-image: url("/src/assets/pattern_circles-7_1_2_0-0_0_1__ffffff_dedede.
     align-items: center;
   }
 
+  .main-title h1:hover {
+    text-shadow: 20px 20px 0px #fb1b1b, 30px 30px 0px #ffcc00,
+      40px 40px 0px rgb(0 117 190);
+    transition: all ease-in-out 250ms;
+  }
+  .main-title h1 {
+    font-size: 6rem;
+    font-weight: bold;
+    margin-bottom: 4rem;
+  }
 
-.main-title h1:hover {
-  text-shadow: 20px 20px 0px #FB1B1B, 30px 30px 0px #FFCC00,
-    40px 40px 0px rgb(0 117 190);
-  transition: all ease-in-out 250ms;
-}
-.main-title h1 {
-  font-size: 6rem;
-  font-weight: bold;
-  margin-bottom: 4rem;
-}
-
-.main-title h3 {
-  font-size: 2rem;
-  margin-bottom: 2rem;
-}
-`
+  .main-title h3 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+  }
+`;
